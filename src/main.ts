56c4aa29ac50e1608d05ceeae6291854b7102f65
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-12-01 01:58:44 +0330

    test welcome form

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import "./assets/styles/index.css";
 import "./assets/styles/animation.css";
 import "./assets/styles/custom.css";
+const stars = document.querySelectorAll<HTMLElement>(".star");
+const loading = document.querySelector<HTMLDivElement>("#loading");
+const welcomingForm = document.querySelector<HTMLDivElement>("#welcomingForm");
+const usernameInput =
+  document.querySelector<HTMLInputElement>("#usernameInput");
 
 const welcomeMessage =
   document.querySelector<HTMLHeadingElement>("#welcomeMessage");
 
 window.addEventListener("load", () => {
+  setTimeout(() => {
+    loading?.classList.add("hidden");
+    welcomingForm?.classList.replace("hidden", "flex");
+  }, 3000);
   setWelcomeMessage();
+  createStar();
+});
+
+window.addEventListener("resize", () => {
+  setWelcomeMessage();
+  createStar();
 });
 
 const setWelcomeMessage = () => {
@@ -19,4 +34,21 @@ const setWelcomeMessage = () => {
     }
   }
 };
-window.addEventListener("resize", setWelcomeMessage);
+
+const createStar = () => {
+  stars.forEach((e, index) => {
+    let x: number = Math.floor(Math.random() * window.innerWidth + 50);
+    let y: number = Math.floor(Math.random() * window.innerHeight + 50);
+    stars[index].style.left = `${x}px`;
+    stars[index].style.top = `${y}px`;
+    index % 2 === 0 ? stars[index].classList.add("animate-ping") : null;
+    console.log(x, y);
+  });
+};
+
+document.querySelector("form")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert(usernameInput?.value);
+  welcomingForm?.classList.add("animation-remove");
+  createStar()
+});
