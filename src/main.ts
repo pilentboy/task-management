8a881fca9892c3fa2485613687d9f1946d888569
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-12-01 00:48:00 +0330

    create register form

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
 import "./assets/styles/index.css";
 import "./assets/styles/animation.css";
 import "./assets/styles/custom.css";
+
+const welcomeMessage =
+  document.querySelector<HTMLHeadingElement>("#welcomeMessage");
+
+window.addEventListener("load", () => {
+  setWelcomeMessage();
+});
+
+const setWelcomeMessage = () => {
+  if (welcomeMessage) {
+    if (window.innerWidth < 640) {
+      welcomeMessage.textContent = "به اپ برنامه ریزی خودت خوش اومدی";
+    } else {
+      welcomeMessage.textContent =
+        "با این اپلیکیشن میتونی به سادگی واسه اهدافت برنامه ریزی کنی";
+    }
+  }
+};
+window.addEventListener("resize", setWelcomeMessage);
