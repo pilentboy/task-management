2269b8ddb1887c89b2d9d8456a2d2529190a9077
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-12-06 00:23:26 +0330

    add hover to profile & group boxes

--- src/components/groupBox.ts
+++ src/components/groupBox.ts
@@ -2,14 +2,14 @@ const renderGroupBox = () => {
   const groupBox = document.createElement("div");
   groupBox.id = "groupBox";
   groupBox.className =
-    "w-[70%] h-36 flex flex-col relative items-center justify-center  sm:h-48 border-[4px] p-4 rounded-xl border-slate-900 overflow-hidden sm:w-48 duration-300";
+    "w-[70%] h-36 flex flex-col relative items-center justify-center  sm:h-48 border-[4px] p-4 rounded-xl border-slate-900 overflow-hidden sm:w-48 duration-300 hover:scale-110 cursor-pointer";
   const groupBoxToggler = document.createElement("button");
   groupBoxToggler.id = "groupBoxToggler";
   groupBoxToggler.type = "button";
   const icon = document.createElement("img");
   icon.src = "/task-management/svg/files-svgrepo-com.svg";
   groupBoxToggler.className =
-    "duration-500 w-28 cursor-pointer absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex items-center justify-center";
+    "duration-500 w-28 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex items-center justify-center";
   groupBoxToggler.append(icon);
   groupBox.append(groupBoxToggler);
   return groupBox;
--- src/components/profileBox.ts
+++ src/components/profileBox.ts
@@ -2,14 +2,14 @@ const renderProfileBox = () => {
   const profileBox = document.createElement("div");
   profileBox.id = "profileBox";
   profileBox.className =
-    "w-[70%] h-36 flex flex-col relative items-center justify-center  sm:h-48 border-[4px] p-4 rounded-xl border-slate-900 overflow-hidden sm:w-48 duration-300";
+    "w-[70%] h-36 flex flex-col relative items-center justify-center  sm:h-48 border-[4px] p-4 rounded-xl border-slate-900 overflow-hidden sm:w-48 duration-300 hover:scale-110 cursor-pointer";
   const profileBoxToggler = document.createElement("button");
   profileBoxToggler.id = "groupBoxToggler";
   profileBoxToggler.type = "button";
   const icon = document.createElement("img");
   icon.src = "/task-management/svg/profile-svgrepo-com.svg";
   profileBoxToggler.className =
-    "duration-500 w-20 cursor-pointer absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex items-center justify-center";
+    "duration-500 w-20  absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex items-center justify-center";
   profileBoxToggler.append(icon);
   profileBox.append(profileBoxToggler);
   return profileBox;
