4e402e4aecfbe50b1d4a72a14c0d94491942dabd
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-12-15 03:52:22 +0330

    add dataset-theme attribute to input radio

--- src/components/settings.ts
+++ src/components/settings.ts
@@ -1,7 +1,7 @@
 const settings = () => {
   const settingsContainer = document.createElement("div");
   settingsContainer.className =
-    "rounded-lg w-[600px] h-fit p-4 bg-black border-2 space-y-2  border-slate-800";
+    "rounded-lg w-[90%] sm:w-[600px] h-fit p-4 bg-black border space-y-2  border-slate-800";
 
   settingsContainer.innerHTML = `
   <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
@@ -12,13 +12,13 @@ const settings = () => {
   <div class="form-control">
   <label class="label cursor-pointer">
     <span class="label-text">ساده</span>
-    <input type="radio" name="uiperformance" class="radio checked:bg-blue-600" checked="checked" />
+    <input type="radio" name="uiperformance" class="radio checked:bg-blue-600" data-theme='ساده'  />
   </label>
 </div>
 <div class="form-control">
   <label class="label cursor-pointer">
     <span class="label-text">حلقه</span>
-    <input type="radio" name="uiperformance" class="radio checked:bg-blue-600" />
+    <input type="radio" name="uiperformance" class="radio checked:bg-blue-600" data-theme='حلقه' />
   </label>
 </div>
   </div>
--- src/utils/handleUIDisplay.ts
+++ src/utils/handleUIDisplay.ts
@@ -2,10 +2,14 @@ const handleUIDisplay = () => {
   const uiperformanceRadios = document.querySelectorAll(
     'input[name="uiperformance"]'
   );
+  
+  const uiperformance=localStorage.getItem("ui_performance")
 
   uiperformanceRadios.forEach((radio) => {
-    radio.addEventListener("click", (e: any) => {
-      const selectedValue = e.target.previousElementSibling.textContent.trim();
+	if(uiperformance && uiperformance === radio.dataset.theme) radio.setAttribute('checked','checked')
+	console.log(radio.dataset.theme)
+    radio.addEventListener("change", (e: any) => {
+      const selectedValue = e.target.dataset.theme;
       localStorage.setItem('ui_performance', selectedValue);
     });
   });
