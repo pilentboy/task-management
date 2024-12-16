a423d01630b8cc8b7e52b8b053ad459f6e639b15
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-12-17 02:26:09 +0330

    refactor handleDeleteGroup

--- src/utils/handleDeleteGroup.ts
+++ src/utils/handleDeleteGroup.ts
@@ -1,27 +1,30 @@
 import { renderTaskList } from "../components/taskManager";
 
 const handleDeleteGroup = (): void => {
+  // Retrieve stored groups and the currently selected group
   const groupData = localStorage.getItem("groups");
-
   const deletedGroup = localStorage.getItem("group_filter");
 
-  if (groupData) {
-    const groupDataObj = JSON.parse(groupData);
-    if (deletedGroup) {
-      const updatedGroup = groupDataObj.filter(
-        (group: any) => Object.keys(group)[0] !== deletedGroup
-      );
-      
-      const firstGroup=Object.keys(updatedGroup[0])[0]
-      
-      localStorage.setItem("group_filter", firstGroup);
-      localStorage.setItem("groups", JSON.stringify(updatedGroup));
-      document.querySelector("#filterBoxContainer")?.remove();
-      document.querySelector("#taskList")?.remove();
-      renderTaskList(firstGroup);
-    }
-  }
+  if (!groupData || !deletedGroup) return; // Exit if required data is missing
 
+  if (groupData && deletedGroup) {
+    const groupDataObj = JSON.parse(groupData);
+
+    // Filter out the group to be deleted
+    const updatedGroups = groupDataObj.filter(
+      (group: Record<string, any>) => Object.keys(group)[0] !== deletedGroup
+    );
+
+    // set the first group as the new active filter
+    const firstGroup = Object.keys(updatedGroups[0])[0];
+    localStorage.setItem("group_filter", firstGroup);
+    localStorage.setItem("groups", JSON.stringify(updatedGroups));
+
+    // Clean up the UI and re-render the task list for the first group
+    document.querySelector("#filterBoxContainer")?.remove();
+    document.querySelector("#taskList")?.remove();
+    renderTaskList(firstGroup);
+  }
 };
 
 export default handleDeleteGroup;
